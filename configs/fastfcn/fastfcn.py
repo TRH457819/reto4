89c80ef39870ff23f7a39d6c921a854ab4ae9bad
Trh7101 <97247188+Trh7101@users.noreply.github.com>
2024-06-06 15:52:42 -0600

    Fastfcn and unet

--- configs/fastfcn/fastfcn.py
+++ configs/fastfcn/fastfcn.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/fcn_unet_s5-d16.py', '../_base_/datasets/retotec_dataset.py',
+    '../_base_/models/fastfcn_r50-d32_jpu_psp.py', '../_base_/datasets/retotec_dataset.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_5k.py'
 ]
 crop_size = (512, 1024)
